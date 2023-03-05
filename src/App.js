627e7a1744f658d9d8f59d207fc3c85b06e31218
GORD22 <andreyanov.15@mail.ru>
2023-03-05 16:06:00 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import DateSlider from "./components/DateSlider/DateSlider";
+
+
+const App = () => {
+    const minDate = new Date(2015,0);
+    const maxDate = new Date(2017, 0);
+    const startDate = new Date(2015,5);
+    const endDate = new Date(2016, 0);
+
+    return (
+        <div className={"App"}>
+            <DateSlider minDate={minDate} maxDate={maxDate}
+                        startDate={startDate} endDate={endDate}
+            />
+        </div>
+    )
+};
+
+export default App;
---
+++ src/components/DateSlider/DateSlider.js
@@ -0,0 +1,96 @@
+import React, {useState} from "react";
+import style from "./DateSlider.module.css";
+import ClassesSlider from "./SliderWithSettings";
+import {labelMonth, sliderMonth} from "../common/months";
+import cn from "classnames";
+
+const DateSlider = ({minDate, maxDate, startDate, endDate}) => {
+    const getDatesInRange = (minDate, maxDate, toggleValue) => {
+        const date = new Date(minDate.getTime());
+        const dates = [];
+        let sliderValue = 0;
+
+        while (date <= maxDate) {
+            if ((new Date(date)).getMonth() === 0) {
+                dates.push({
+                    value: sliderValue, label: `${(new Date(date)).getFullYear()}`
+                })
+            } else {
+                switch (toggleValue) {
+                    case false:
+                        dates.push({
+                            value: sliderValue
+                        })
+                        break;
+                    case true:
+                        dates.push({
+                            value: sliderValue,
+                            label: `${sliderMonth[(new Date(date)).getMonth()]}`
+                        })
+                        break;
+                    default:
+                        return
+                }
+            }
+            sliderValue++;
+            date.setMonth(date.getMonth() + 1);
+        }
+        return dates;
+    };
+    const valueLabelFormat = value => {
+        const date = new Date(minDate.getTime());
+        date.setMonth(date.getMonth() + value);
+
+        return `${labelMonth[date.getMonth()]} ${date.getFullYear()}`
+    };
+    const renderSwitch = (isToggle) => {
+        switch (isToggle) {
+            case false:
+                return <ClassesSlider date={date}
+                                      updateRange={updateRange}
+                                      items={getDatesInRange(minDate, maxDate, false)}
+                                      valueLabelFormat={valueLabelFormat}
+                                      isToggle={isToggle}
+                />
+            case true:
+                return <ClassesSlider date={date}
+                                      updateRange={updateRange}
+                                      items={getDatesInRange(minDate, maxDate, true)}
+                                      valueLabelFormat={valueLabelFormat}
+                                      isToggle={isToggle}
+                />
+            default:
+                return
+        }
+    }
+
+    const [date, setDate] = useState([
+        (startDate.getFullYear() - minDate.getFullYear())*12 + startDate.getMonth() - minDate.getMonth(),
+        (endDate.getFullYear() - minDate.getFullYear())*12 + endDate.getMonth() - minDate.getMonth()
+    ]);
+    const updateRange = (e, value) => {
+        setDate(value)
+    };
+
+    const [toggleSlider, setToggleSlider] = useState(false);
+
+    return (
+        <div className={style.substrate}>
+            <div className={style.slider}>
+                <div>
+                    <div onClick={() => setToggleSlider(false)}
+                         className={!toggleSlider ?
+                             cn(style.switchElement, style.activeSwitchElement) :
+                             style.switchElement}>Все года</div>
+                    <div onClick={() => setToggleSlider(true)}
+                         className={toggleSlider ?
+                             cn(style.switchElement, style.activeSwitchElement) :
+                             style.switchElement}>Месяца</div>
+                </div>
+                    {renderSwitch(toggleSlider)}
+            </div>
+        </div>
+    )
+}
+
+export default DateSlider;
